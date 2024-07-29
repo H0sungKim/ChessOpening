128ed137d0b8fcd86f4350517b979f60ad3c682d
Hosung.Kim <hyongak516@mail.hongik.ac.kr>
2024-07-29 14:13:23 +0900

    Delete iOS/Pods directory

--- iOS/Pods/Target Support Files/Pods-ChessOpening-ChessOpeningUITests/Pods-ChessOpening-ChessOpeningUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ChessOpening_ChessOpeningUITests : NSObject
-@end
-@implementation PodsDummy_Pods_ChessOpening_ChessOpeningUITests
-@end
--- iOS/Pods/Target Support Files/Pods-ChessOpeningTests/Pods-ChessOpeningTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ChessOpeningTests : NSObject
-@end
-@implementation PodsDummy_Pods_ChessOpeningTests
-@end
